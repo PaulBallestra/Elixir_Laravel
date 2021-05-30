bd2cb8318d0bd14fffde10da1ab2cc9cce92d80b
PaulBallestra <paulballestra@yahoo.fr>
2021-05-30 16:38:51 +0200

    Added routes : Services/Contact/Login/Register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+//ROUTES
+
+//Accueil
 Route::get('/', function () {
     return view('welcome');
+});
+
+//SERVICES
+Route::get('/services', function() {
+    return view('services');
+});
+
+//CONTACT
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+//LOGIN
+Route::get('/login', function () {
+    return view('login');
+});
+
+//REGISTER
+Route::get('/register', function () {
+    return view('register');
 });
