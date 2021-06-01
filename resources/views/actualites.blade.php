773f07fc203874938634c25fef1a1c3c26a44e5b
PaulBallestra <paulballestra@yahoo.fr>
2021-06-01 12:18:56 +0200

    Added the actuality post

--- resources/views/actualites.blade.php
+++ resources/views/actualites.blade.php
@@ -20,7 +20,7 @@
             <livewire:header />
 
             <!-- TITLE DE LA PAGE -->
-            <div class="grid grid-cols-6 gap-4">
+            <div class="grid grid-cols-6 gap-4 mb-4">
                 <div class="col-start-2 col-span-4">
                     <h1 class="titleCustomClass mt-4 text-center"> ACTUALITÉS </h1>
                     <h3 class="sousTitleCustomClass text-center"> Retrouvez toutes nos actualités par date de sortie. </h3>
@@ -28,9 +28,41 @@
             </div>
 
             <!-- GRID DES ACTUALITÉS -->
-            <div class="grid grid-cols-3 md:grid-cols-8 md:gap-4">
-                <div class="col-start-1 md:col-start-2 col-end-4 ...">1</div>
-                <div class="col-start-1 md:col-start-5 col-end-8 ...">2</div>
+            <div class="grid grid-cols-3 sm:grid-cols-4 sm:gap-4">
+
+                <!-- ACTU 1 -->
+                <div class="col-start-1 sm:col-start-1 col-end-3">
+
+                    <!-- SQUELETTE ACTUALITE -->
+                    <livewire:actualite />
+
+                </div>
+
+                <!-- ACTU 2 -->
+                <div class="col-start-1 sm:col-start-3 col-end-5">
+
+                    <div class="divActualite" style="background-image: url({{asset('images/guitarlotof.jpg')}})">
+
+                        <div class="divActualiteInfos">
+
+                            <p class="actualiteDate"> 24/04/2021 </p>
+                            <h3 class="actualiteTitle"> VOTRE GUITARE MÉRITE MIEUX </h3>
+                            <div class="grid grid-cols-4 gap-4">
+                                <p class="actualiteContent col-start-1 col-end-4">
+                                    Grâce aux cordes Elixir, votre jeu ne sera que
+                                    plus qualitatif. Une durée de vie allongée,
+                                    et une tenue d'accordage solide.
+                                    Votre guitare vous remerciera !
+                                </p>
+                            </div>
+
+                            <div class="flex flex-column">
+                                <p style="align-items: flex-end" class="actualiteBtnSuite"> Lire la suite >> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
             @livewireScripts
---
+++ resources/views/livewire/actualite.blade.php
@@ -0,0 +1,20 @@
+<div class="divActualite" style="background-image: url({{asset('images/type_elixir.png')}})">
+
+    <div class="divActualiteInfos">
+
+        <p class="actualiteDate"> 16/05/2021 </p>
+        <h3 class="actualiteTitle"> NOUVELLES CORDES 10-46 </h3>
+        <div class="grid grid-cols-4 gap-4">
+            <p class="actualiteContent col-start-1 col-end-4"> Les nouvelles cordes Elixir 10 - 46 pour
+                guitare électrique sont arrivées !
+                Le revêtement Elixir est toujours présent
+                et vos cordes n'auront jamais tenu
+                aussi longtemps.
+            </p>
+        </div>
+
+        <div class="flex flex-column">
+            <p style="align-items: flex-end" class="actualiteBtnSuite"> Lire la suite >> </p>
+        </div>
+    </div>
+</div>
