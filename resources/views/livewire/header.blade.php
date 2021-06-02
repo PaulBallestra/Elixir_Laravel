4ca9aea8de2e2bfc35cd90db3dbd4a158cd4c709
PaulBallestra <paulballestra@yahoo.fr>
2021-06-02 22:38:47 +0200

    Change selected title of page on header

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -42,26 +42,28 @@
                 </a>
                 <div class="hidden sm:block sm:ml-6">
                     <div class="flex space-x-4">
+
+                        {{ Request::segment(1) }}
+
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/actualites"
-                           class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass"
-                           aria-current="page">ACTUALITÉS</a>
+                           class="hover:bg-gray-700 text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass @if(Request::segment(1) === 'actualites') bg-gray-900 text-white  @endif">ACTUALITÉS</a>
 
                         <a href="/service"
-                           class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass">SERVICE</a>
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass @if(Request::segment(1) === 'service') bg-gray-900 text-white @endif">SERVICE</a>
 
                         <a href="/contact"
-                           class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass">CONTACT</a>
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass @if(Request::segment(1) === 'contact') bg-gray-900 text-white @endif">CONTACT</a>
 
                         @if (Route::has('login'))
                             @auth
                                 <a href="/abonnement"
-                                   class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass">ABONNEMENT</a>
+                                   class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass @if(Request::segment(1) === 'abonnement') bg-gray-900 text-white @endif">ABONNEMENT</a>
 
                                 <a href="/logout"
                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans titleHeaderClass">LOGOUT</a>
 
-                                <a href="/profile" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans"><svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/></svg> </a>
+                                <a href="/profile" class="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-sans @if(Request::segment(1) === 'profile') bg-gray-900 @endif"><svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/></svg> </a>
 
                             @else
 
